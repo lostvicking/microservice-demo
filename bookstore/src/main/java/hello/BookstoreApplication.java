70bdbb667a80e8102b90e6f06d74c6e1502a6168
Victor Neiman <lostvicking@gmail.com>
2017-11-16 21:36:29 +0100

    Spring Boot Skeleton App

--- bookstore/src/main/java/hello/BookstoreApplication.java
+++ bookstore/src/main/java/hello/BookstoreApplication.java
@@ -8,21 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@RestController
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookstoreApplication {
-
-    @Value("${readingList}")
-    private String readingList;
-
-    @RequestMapping(value = "/recommended", method = RequestMethod.GET)
-    public String getReadingList(){
-        return readingList;
-    }
-
-    // java -jar target/bookstore-1.0-SNAPSHOT.jar --readingList="Wut" --server.port=8091
-
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
     }
